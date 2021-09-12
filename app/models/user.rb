219b35749f8ac031fdaa1dc44147229e3a613860
hana-0612 <hyania1206@gmail.com>
2021-09-12 18:44:48 +0900

    validatesの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   with_options presence: true do
+    validates :nickname
     validates :date_of_birth
     with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/} do
       validates :last_name
