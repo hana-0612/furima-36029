1c759318a415e6efa55c0000275d19472765a99a
hana-0612 <hyania1206@gmail.com>
2021-09-12 18:31:34 +0900

    新規登録の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
-  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
 
   private
@@ -10,4 +10,9 @@ class ApplicationController < ActionController::Base
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
   end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name, :first_name, :kana_last_name,  :kana_first_name, :date_of_birth])
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :date_of_birth
+    with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/} do
+      validates :last_name
+      validates :first_name
+    end
+ 
+    with_options format: { with: /\A[ァ-ヶー]+\z/ } do
+      validates :kana_last_name
+      validates :kana_first_name
+    end
+  end
+
 end
