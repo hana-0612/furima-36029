9d4843519b7d2c7179407ca4e3178a254e2fedba
hana-0612 <hyania1206@gmail.com>
2021-09-13 17:07:08 +0900

    Active Storage導入

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,9 @@
 class ProductsController < ApplicationController
   def index
   end
+
+  private
+  def product_params
+    parrams.require(:product).permit(:title, :product_description, :category_id, :product_condition_id, :shipping_charge_id, :shipping_area_id, :shipping_ship_id, :price :image).merge(user_id: current_user.id)
+  end
 end
