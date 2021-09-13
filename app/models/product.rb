2085e71111efe9ef6581e1e562ba0cc680723f6a
hana-0612 <hyania1206@gmail.com>
2021-09-13 19:46:20 +0900

    productモデルにバリテーションの記述

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,15 @@ class Product < ApplicationRecord
   belong_to :user
   has_one_attached :image
 
+  with_options presence: true do
+    validates :title
+    validates :product_description
+    validates :category_id
+    validates :product_condition_id
+    validates :shipping_charge_id
+    validates :shipping_area_id
+    validates :shipping_ship_id
+    validates :price
+  end
+
 end
