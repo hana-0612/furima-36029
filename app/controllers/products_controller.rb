ae4dbf0ea2f9f94e6cd06d3c62b775c09ca8cb5a
hana-0612 <hyania1206@gmail.com>
2021-09-13 20:57:15 +0900

    ストロングパラメータ記述修正

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,8 +6,17 @@ def new
   @product = Product.new
 end
 
+def create
+  @product = Product.new(product_params)
+  if @product.save
+    redirect_to root_path(@product)
+  else
+    render :new
+  end
+end
+
   private
   def product_params
-    params.require(:product).permit(:title, :product_description, :category_id, :product_condition_id, :shipping_charge_id, :shipping_area_id, :shipping_ship_id, :price :image).merge(user_id: current_user.id)
+    params.require(:product).permit(:title, :product_description, :category_id, :product_condition_id, :shipping_charge_id, :shipping_area_id, :shipping_ship_id, :price, :image).merge(user_id: current_user.id)
   end
 end
