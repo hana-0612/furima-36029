15a6dee3dca35b90a00f0a11270065bf829dd72d
hana-0612 <hyania1206@gmail.com>
2021-09-19 00:42:25 +0900

    コントローラの設定

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,8 @@
 class ProductsController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
+
   def index
+    @products = Product.all.order("created_at DESC")
   end
 
 def new
@@ -8,8 +11,9 @@ end
 
 def create
   @product = Product.new(product_params)
-  if @product.save
-    redirect_to root_path(@product)
+  if @product.valid?
+    @product.save
+    redirect_to root_path
   else
     render :new
   end
