388b1cafb814590165de0d1686812f4ac103027b
hana-0612 <hyania1206@gmail.com>
2021-09-19 00:33:19 +0900

    ActiveHashの導入

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,17 +1,22 @@
 class Product < ApplicationRecord
-  belong_to :user
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :user
   has_one_attached :image
+  belongs_to_active_hash :category
+  belongs_to_active_hash :product_condition
+  belongs_to_active_hash :shipping_charge
+  belongs_to_active_hash :shipping_area
+  belongs_to_active_hash :shipping_ship
 
   with_options presence: true do
-    validates :title
-    validates :product_description
-    validates :category_id
-    validates :product_condition_id
-    validates :shipping_charge_id
-    validates :shipping_area_id
-    validates :shipping_ship_id
+    validates :title, :product_description, :image
+
+    with_options numericality: { other_tham: 1 } do
+      validates :category_id, :product_condition_id, :shipping_charge_id,:shipping_area_id,:shipping_ship_id
+    end
+   
+    
     validates :price
-    validates :image
   end
 
 end
