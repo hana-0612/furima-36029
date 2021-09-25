b81690bdbbf9dc66c8b15d106762b9cb9ca08687
hana-0612 <hyania1206@gmail.com>
2021-09-25 17:11:08 +0900

    コントローラーの設定

---
+++ app/controllers/purchase_records_controller.rb
@@ -0,0 +1,34 @@
+class PurchaseRecordsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :product_find
+
+  def index
+    if current_user.id != @product_user_id && @product.purchase_record == nil
+      @purchase_record_address = PurchaseRecordAddress.new
+    else
+      redirect_to root_path
+    end
+  end
+
+  def create
+    
+    @purchase_record_address = PurchaseRecordAddress.new(purchase_record_params)
+    if @purchase_record_address.valid?
+      @purchase_record_address.save
+      redirect_to root_path
+    else
+      render action: :index
+    end
+  end
+
+
+  private
+
+  def product_find
+    @product = Product.find(params[:product_id])
+  end
+
+  def purchase_record_params
+    params.require(:purchase_record_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :telephone_number).merge(product_id: params[:product_id], user_id: current_user.id)
+  end
+end
