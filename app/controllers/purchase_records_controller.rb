e26bf6be7206e9ce6bc565f26f283b0a6640473e
hana-0612 <hyania1206@gmail.com>
2021-09-26 02:36:38 +0900

    PAY.JP設定

--- app/controllers/purchase_records_controller.rb
+++ app/controllers/purchase_records_controller.rb
@@ -3,7 +3,7 @@ class PurchaseRecordsController < ApplicationController
   before_action :product_find
 
   def index
-    if current_user.id != @product_user_id && @product.purchase_record == nil
+    if current_user.id != @product.user_id && @product.purchase_record == nil
       @purchase_address = PurchaseAddress.new
     else
       redirect_to root_path
@@ -13,6 +13,7 @@ class PurchaseRecordsController < ApplicationController
   def create
     @purchase_address = PurchaseAddress.new(purchase_record_params)
     if @purchase_address.valid?
+      pay_item
       @purchase_address.save
       return redirect_to root_path
     else
@@ -27,8 +28,19 @@ class PurchaseRecordsController < ApplicationController
   end
 
   def purchase_record_params
-    params.require(:purchase_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :telephone_number).merge(
-      product_id: params[:product_id], user_id: current_user.id
+    params.require(:purchase_address).permit(
+      :postal_code, :prefecture_id, :city, :house_number, :building_name, :telephone_number
+    ).merge(
+      token: params[:token], product_id: params[:product_id], user_id: current_user.id
+    )
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @product.price,
+      card: purchase_record_params[:token],
+      currency: 'jpy'
     )
   end
 end
--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -1,10 +1,10 @@
 class PurchaseAddress
   include ActiveModel::Model
   attr_accessor :user_id, :product_id, :postal_code, :prefecture_id, :city, :house_number, :building_name, :telephone_number,
-                :purchase_record_id
+                :purchase_record_id, :token
 
   with_options presence: true do
-    validates :city, :house_number
+    validates :city, :house_number, :token
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
     validates :prefecture_id, numericality: { other_than: 1 }
     validates :telephone_number, presence: true, format: { with: /\A[0-9]{10,11}\z/ }
